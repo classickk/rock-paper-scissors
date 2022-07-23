4d2d84901ae3a6e7800a316b20073deb82d476c7
Ben Hultum <benhultum@icloud.com>
2022-07-23 18:19:26 +0100

    Add playRound function
    Ensures player selection is a valid format
    Checks validity of player selection
    Checks winner or round if player selection valid

--- javascript.js
+++ javascript.js
@@ -1,22 +1,23 @@
 function getComputerChoice() {
     // generate random number between 0 - 2
-    const RAN_NUM = Math.floor(Math.random() * (2 + 1));
+    const ranNum = Math.floor(Math.random() * (2 + 1));
     // initialise variable to hold computer choice
     let compChoice;
     // assign 'Rock', 'Paper' or 'Scissors' to compChoice
-    switch(RAN_NUM) {
+    switch(ranNum) {
         case 0:
             compChoice = "Rock";
             break;
         case 1:
-            compChoice="Paper";
+            compChoice = "Paper";
             break;
         case 2:
-            compChoice="Scissors";
+            compChoice = "Scissors";
             break;
     }
     return compChoice;
 }
+
 
 function playRound(playerSelection, computerSelection) {
     // remove leading and trailing spaces from player selection
@@ -24,16 +25,45 @@ function playRound(playerSelection, computerSelection) {
     // make first letter of player choice uppercase
     let firstLetter = playerSelection.charAt(0).toUpperCase();
     // make rest of player choice lowercase
-    let restOfWord = playerSelection.substr(1).toLowerCase();
+    let restOfName = playerSelection.substr(1).toLowerCase();
     // rebuild player choice string
-    playerSelection = firstLetter.concat(restOfWord);
+    playerSelection = firstLetter.concat(restOfName);
 
+    // check player selection is valid
+    let isValid;
+    if(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors") {
+        isValid = true;
+    } else {
+        isValid = false;
+    }
 
-
-    console.log(playerSelection);
-    console.log(computerSelection);
+    // if player selection is valid
+    if((isValid)) {
+        // check for draw if player and computer make the same selection
+        if(playerSelection === computerSelection) {
+            return `It's a draw!`
+        }
+        // check player wins
+        else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+                playerSelection === "Paper" && computerSelection === "Rock" ||
+                playerSelection === "Scissors" && computerSelection === "Paper") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+        // if player does not win and game is not a draw, computer wins
+        else {
+            return `You lose! ${computerSelection} beats ${playerSelection}`
+        }
+    }
+    // return notification if player selection is not valid
+    else {
+        return `"${playerSelection}" is not a valid selection. Please choose either Rock, Paper or Scissors`;
+    }
 }
 
-const playerSelection = "roCk";
+function game() {
+    
+}
+
+const playerSelection = "elephant";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
